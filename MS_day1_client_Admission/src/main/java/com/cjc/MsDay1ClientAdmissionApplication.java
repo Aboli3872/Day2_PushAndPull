7516daf1c9517d298aa87a8a1c2db7acffe2ca02
Dell <Dell@DESKTOP-ND1EAOV>
2024-05-16 16:02:26 +0530

    commit by aboli

---
+++ MS_day1_client_Admission/src/main/java/com/cjc/MsDay1ClientAdmissionApplication.java
@@ -0,0 +1,17 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsDay1ClientAdmissionApplication {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is ms client admisssion");
+		SpringApplication.run(MsDay1ClientAdmissionApplication.class, args);
+	}
+
+}
