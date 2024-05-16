1d387d0bdf971b2ac864a56ffffb4c7a62107ef1
Aboli3872 <168178760+Aboli3872@users.noreply.github.com>
2024-05-16 16:19:03 +0530

    changes to main method

--- MS_day1_client_Admission/src/main/java/com/cjc/MsDay1ClientAdmissionApplication.java
+++ MS_day1_client_Admission/src/main/java/com/cjc/MsDay1ClientAdmissionApplication.java
@@ -11,6 +11,7 @@ public class MsDay1ClientAdmissionApplication {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is ms client admisssion");
+		System.out.println("This is Admission Client");
 		SpringApplication.run(MsDay1ClientAdmissionApplication.class, args);
 	}
 
